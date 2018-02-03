53d0046049ae71c78308f66d33656812f6606a9a
lovelyappleHoge <lovelyooapple@gmail.com>
2018-02-03 17:33:19 +0900

    [1.0.0.0]camerawork追加

---
+++ Assets/Script/UI/CameraController/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject lookAtTarget;
+    [SerializeField] GameObject offsetTarget;
+    [SerializeField] float distacneDump = 10.0f;
+    [SerializeField] Camera targetCamera;
+	Vector3 offseTargetPos;
+    Vector3 velocity = Vector3.one;
+    public bool lookAtTaretFlag = true;
+
+    public void SetupCamera(GameObject targetObj, GameObject offsetObj)
+    {
+        this.lookAtTarget = targetObj;
+        this.offsetTarget = offsetObj;
+    }
+    void Update()
+    {
+        if (targetCamera == null)
+        {
+            Debug.LogError("could not find camera");
+            return;
+        }
+        LookAtTarget();
+        FollowTheOffseTarget();
+    }
+    void FollowTheOffseTarget()
+    {
+        if (offsetTarget == null) { return; }
+        var curPos = Vector3.SmoothDamp(targetCamera.transform.position, offsetTarget.transform.position, ref velocity, distacneDump);
+        targetCamera.transform.position = curPos;
+    }
+    void LookAtTarget()
+    {
+        if (lookAtTaretFlag)
+            targetCamera.transform.LookAt(lookAtTarget.transform.position, Vector3.up);
+    }
+}
