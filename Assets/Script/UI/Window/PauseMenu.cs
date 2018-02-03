53cf05c4d0ecff620cfd30a81952f87aa3dce76a
inagawa <bluemoonlight0340@gmail.com>
2018-02-03 19:51:04 +0900

    [1.0.0.0]ポーズメニュー追加変更

--- Assets/Script/UI/Window/PauseMenu.cs
+++ Assets/Script/UI/Window/PauseMenu.cs
@@ -1,9 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : WindowBase {
-
+	public Sprite Howplay;
+	public Sprite HowgGame;
 	// Use this for initialization
+
+	private int Mode = 0;
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			if (Mode < 1) {
+				Mode += 1;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			if (Mode > 0) {
+				Mode -= 1;
+			}
+		}
+
+		switch(Mode){
+		case 0:
+			gameObject.GetComponent<Image> ().sprite = Howplay;
+			break;
+
+		case 1:
+			gameObject.GetComponent<Image> ().sprite = HowgGame;
+			break;
+
+		}
+	}
 
 }
