597330fc0232fc3d0ca357b76f1ee5b10490d3d8
lovelyappleHoge <lovelyooapple@gmail.com>
2018-02-12 14:04:30 +0900

    [1.2.0.0]damageArea,GamesettingTable,result_pause

--- Assets/Script/Manager/FxManager.cs
+++ Assets/Script/Manager/FxManager.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //todo resourceManagerでキャッシュ化
+/* /*
+// 				/l__/l           食料くれにゃー
+			   /  _  _l           
+		      /     w /__
+		     /    ____C __C 
+	_______/       /
+	~~~~~~       /
+	     |  / | / 
+		 | /  |/
+	 
+	*/
 public class FxManager : SingleToneBase<FxManager>
 {
     [SerializeField] GameObject fxRoot;
@@ -23,6 +34,7 @@ public class FxManager : SingleToneBase<FxManager>
                 return;
             }
         }
+
         var goC = GameObject.Instantiate(fxControlerPrefab, fxRoot.transform);
         goC.transform.position = pos;
         var fxC = goC.GetComponent<FxController>();
---
+++ Assets/Script/Table/GameSettingTable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * マジックナンバーだけは避けたい
+ * 同じくcsv面倒いのでそのまま書き込む
+ */
+public static class GameSettingTable
+{
+    //
+    //変更してはいけないもの
+    //
+    ///キャラステータス：重さを計算するための係数
+    public static float CharacStatusWeightMax { get { return 1000; } }
+
+    //
+    //変更していいもの
+    //
+
+    ///キャラ:移動加速力
+    public static float CharaStatusSpeedAdd { get { return 50f; } }
+    ///キャラ:ダメージ受ける時に、ダンプされる力
+    public static float CharaDamagedDumpPower { get { return 300f; } }
+    ///キャラ:ダメージを受けた後の無敵時間
+    public static float CharaDamageCoolingTime { get { return 2f; } }
+    ///敵：投げられる時の力
+    public static float EnemyPowerThrowOut { get { return 300f; } }
+    ///敵：攻撃時にジャンプする方向ベクトル高さオフセット
+    public static float EnemyAttackOffesetY { get { return 0.3f; } }
+	///敵：メインキャラを捕まった後、くっ付いとく最大距離
+    public static float EnemyReleaseRange { get { return 2.5f; } }
+}
